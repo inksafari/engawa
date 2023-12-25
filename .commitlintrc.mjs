bc3c534530864fca403ca7271cd844697f6f7b2b
inksafari <inksafari@users.noreply.github.com>
2023-12-25 23:32:12 +0800

    chore(git-hook): update pre-commit hook
    * build(deps): updated package dependencies
    
    * lefthook: pre-commit hook新增執行Prettier任務
    
    * prettier/eslint/stylelint：.cache資料夾移到node_modules底下
    
    * commitlint: commit msg寬度限制50/72字元

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -2,6 +2,12 @@
 // echo 'Updated package dependencies' | pnpx commitlint
 const config = {
   extends: ['@commitlint/config-conventional'],
-};
+  // @see: https://commitlint.js.org/#/reference-rules
+  rules: {
+    'body-max-line-length': [1, 'always', 72],   // [2, 'always', 120],
+    'footer-max-line-length': [1, 'always', 72], // [2, 'always', 120],
+    'header-max-length': [1, 'always', 50],      // [2, 'always', 72],
+  },
+}
 
 export default config;
--- scripts/clear.mjs
+++ scripts/clear.mjs
@@ -1,8 +1,8 @@
 // @credit: https://github.com/jamband/blog/blob/main/scripts/clear.js
-import { rm } from 'node:fs/promises';
+import { rm } from 'node:fs/promises'
 
-await rm('.astro', { force: true, recursive: true });
-await rm('.cache', { force: true, recursive: true });
-await rm('dist', { force: true, recursive: true });
+await rm('.astro', { force: true, recursive: true })
+await rm('dist', { force: true, recursive: true })
+// await rm('node_modules/.cache', { force: true, recursive: true })
 // await rm('node_modules', { force: true, recursive: true })
-console.info('✅ Done!');
+console.info('✅ Done!')
